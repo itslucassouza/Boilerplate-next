8fc3b1802c45189415fc68c40d124c1aaf9f133c
itslucassouza <lucasvini193@hotmail.com>
2021-12-13 22:21:49 -0300

    [finalized boilerplate-next]

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react'
+
 import Main from '.'
 
 describe('<Main />', () => {
-  it('shoud render the heading', () => {
+  it('should render the heading', () => {
     const { container } = render(<Main />)
 
     expect(
@@ -10,5 +11,11 @@ describe('<Main />', () => {
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
